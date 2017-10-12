7b29498832a774ce34ce2f96d1be30e54179b3cb
Jose Mejia <jamc.mejia@gmail.com>
2017-10-12 15:05:20 -0600

    Adding service class

--- manifests/service.pp
+++ manifests/service.pp
@@ -1,5 +1,17 @@
 # == Class: carbonblack::config
 #
-class carbonblack::service {
+class carbonblack::service(
+  $service_name       = $::carbonblack::service_name,
+  $service_ensure     = $::carbonblack::service_ensure,
+  $service_enable     = $::carbonblack::service_enable,
+  $service_hasrestart = $::carbonblack::service_hasrestart,
+  $service_hasstatus  = $::carbonblack::hasstatus,
+  ) {
   # resources
+  service { $service_name:
+    ensure     => $service_ensure,
+    enable     => $service_enable,
+    hasrestart => $service_hasrestart,
+    hasstatus  => $service_hasstatus,
+  }
 }
